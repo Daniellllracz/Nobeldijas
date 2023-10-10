b7dde45e93d2a163af17dd977788f5377ad0f73c
Dániel Rácz <dracz898@gmail.com>
2023-10-10 08:37:17 +0200

    Add project files.

---
+++ Nobeldijasfasz/Elethossz.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobeldijasfasz
+{
+    class Elethossz
+    {
+        public int Ev { get; set; }
+        public string Nev { get; set; }
+        public string SzuletesHalalozas { get; set; }
+        public string OrszagKod { get; set; }
+
+        public Elethossz(int ev, string nev, string szuletesHalalozas, string orszagKod, int tol, int ig)
+        {
+            Ev = ev;
+            Nev = nev;
+            SzuletesHalalozas = szuletesHalalozas;
+            OrszagKod = orszagKod;
+            Tol = tol;
+            Ig = ig;
+        }
+
+        private int Tol { get; set; }
+            private int Ig { get; set; }
+            public int ElethosszEvekben => Tol == -1 || Ig == -1 ? -1 : Ig - Tol;
+
+            public bool IsmertAzElethossz => ElethosszEvekben != -1;
+
+            public Elethossz(string SzuletesHalalozas)
+            {
+                string[] m = SzuletesHalalozas.Split('-');
+                try
+                {
+                    Tol = int.Parse(m[0]);
+                }
+                catch (Exception)
+                {
+                    Tol = -1;
+                }
+                try
+                {
+                    Ig = int.Parse(m[1]);
+                }
+                catch (Exception)
+                {
+                    Ig = -1;
+                }
+
+
+            }
+        }
+}
+
---
+++ Nobeldijasfasz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Nobeldijasfasz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dijazotak = new List<Dijazotak>();
+            using var sr = new StreamReader(@"..\..\..\src\orvosi_nobeldijak.txt",
+            Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) dijazotak.Add(new(sr.ReadLine()));
+
+            Console.WriteLine($"3.feladat {dijazotak.Count}fő");
+            var f4 = dijazotak.Max(d => d.Ev);
+            Console.WriteLine($"4.feladat {f4}ben");
+            Console.Write("Kérem adja meg egy ország kódját: ");
+            string ok = Console.ReadLine();
+            var f5 = dijazotak.Where(d => d.Orszag == ok).ToList();
+            if (f5.Count == 0) Console.WriteLine("\t A megadott országnak nem volt díjazotja");
+            else if (f5.Count == 1)
+            {
+                Console.WriteLine("\t A megadott országnak dijazotja");
+                Console.WriteLine($"\tNév {f5[0].Nev}");
+                Console.WriteLine($"\tNév {f5[0].Ev}");
+                Console.WriteLine($"\tNév {f5[0].SztSztr}");
+            }
+
+            
+
+
+            
+        }
+    }
+}
